9b75f3207ad0170a499c706096051abb83d2ae58
Priscila <prioolivesouza@hotmial.com>
2023-05-26 18:22:48 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"saiu novo filme",
+    "description": "olá",
+    "photoCover":"'https://th.bing.com/th/id/OIP.F00dCf4bXxX0J-qEEf4qIQHaD6?pid=ImgDet&rs=1"
+  },
+  {
+    "id":"2",
+    "title":"saiu novo filme",
+    "description": "olá",
+    "photoCover":"'https://th.bing.com/th/id/OIP.F00dCf4bXxX0J-qEEf4qIQHaD6?pid=ImgDet&rs=1"
+  },
+  {
+    "id":"3",
+    "title":"saiu novo filme",
+    "description": "olá",
+    "photoCover":"'https://th.bing.com/th/id/OIP.F00dCf4bXxX0J-qEEf4qIQHaD6?pid=ImgDet&rs=1"
+  },
+  {
+    "id":"4",
+    "title":"saiu novo filme",
+    "description": "olá",
+    "photoCover":"'https://th.bing.com/th/id/OIP.F00dCf4bXxX0J-qEEf4qIQHaD6?pid=ImgDet&rs=1"
+  }
+]
+
+
